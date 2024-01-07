f21096adb6eb9ae160b43601ef5c8f5ef66ca3d6
Krishna Jain <krishnajain5050@gmail.com>
2024-01-07 22:34:21 +0000

    Delete scripts directory

--- scripts/make_dbt_block.py
+++
@@ -1,5 +0,0 @@
-from prefect_dbt.cloud import DbtCloudCredentials
-
-dbt_cloud_credentials = DbtCloudCredentials(api_key="add your API", account_id=148529)
-
-dbt_cloud_credentials.save("covid-dbt-creds", overwrite=True)
